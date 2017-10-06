8ba5963b066719f382e4395dbcab88354ccc88c2
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2017-10-06 19:45:54 +0200

    Added view interface and its implementation

--- app/src/main/java/xyz/gonzapico/kc_networking/HomeActivity.java
+++ app/src/main/java/xyz/gonzapico/kc_networking/HomeActivity.java
@@ -2,11 +2,30 @@ package xyz.gonzapico.kc_networking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import xyz.gonzapico.kc_networking.getGithubUser.GithubUserView;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements GithubUserView {
+
+  TextView tvGithubUsername, tvGithubBio;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_home);
+
+    bindViews();
+  }
+
+  @Override public void renderBio(String bio) {
+    tvGithubBio.setText(bio);
+  }
+
+  @Override public void renderUsername(String username) {
+    tvGithubUsername.setText(username);
+  }
+
+  private void bindViews(){
+    tvGithubUsername = (TextView) findViewById(R.id.tvGithubName);
+    tvGithubBio = (TextView) findViewById(R.id.tvGithubBio);
   }
 }
---
+++ app/src/main/java/xyz/gonzapico/kc_networking/getGithubUser/GithubUserView.java
@@ -0,0 +1,12 @@
+package xyz.gonzapico.kc_networking.getGithubUser;
+
+/**
+ * Created by gonzapico on 06/10/2017.
+ */
+
+public interface GithubUserView {
+
+  void renderUsername(String username);
+
+  void renderBio(String bio);
+}
